5fa1fd46b9e2d41841a7fd8915471c2e080b10c4
Khalil Lakhdhar <khalillakhdharatc@gmail.com>
2021-05-29 10:29:46 +0100

    validation

--- src/testing/Validation.java
+++ src/testing/Validation.java
@@ -1,28 +1,29 @@
 package testing;
 
 public class Validation {
-	public boolean verifb(int b)
-	{
-	return b!=0;	
-	
+	public boolean verifb(int b) {
+		return b != 0;
+
 	}
-	public int convert(String x)
-	{
+
+	public int convert(String x) {
 		return Integer.parseInt(x);
-		
+
 	}
-	public boolean compare(int a,int b)
-	{
-		return a>b;
-		
+
+	public boolean compare(int a, int b) {
+		return a > b;
+
 	}
-	public boolean different(int a,int b)
-	
+
+	public boolean different(int a, int b)
+
 	{
-		if(a!=b)
+		if (a != b)
 			return true;
-		else return false;
-		
+		else
+			return false;
+
 	}
- 
+
 }
