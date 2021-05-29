a2c0ac37f12ff6bc761ce651b0afe5ca714e1633
Khalil Lakhdhar <khalillakhdharatc@gmail.com>
2021-05-29 11:22:20 +0100

    Create Testing.java

---
+++ src/testing/Testing.java
@@ -0,0 +1,30 @@
+package testing;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class Testing {
+Validation valider;
+@Before
+public void initialise()
+{
+valider=new Validation();	
+}
+
+@Test
+public void testchaine()
+{
+	assertTrue(valider.verifchaine(" Ali "));
+}
+@Test
+public void testparite()
+{
+assertEquals(true,valider.paire(15));	
+}
+	
+	
+	
+}
